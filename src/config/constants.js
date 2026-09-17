3495c02b8dec53927d107d959e59eb7115ed2267
harshyadav152 <harshyadav.personal01@gmail.com>
2026-09-17 22:15:57 +0530

    src/config constant added

---
+++ src/config/constants.js
@@ -0,0 +1,14 @@
+export const API_PREFIX = "/api/v1";
+
+export const COOKIE_NAMES = {
+    USER_ACCESS_TOKEN: "UaccessToken",
+    HOF_ACCESS_TOKEN: "HaccessToken",
+};
+
+export const ROLES = {
+    USER: "family_member",
+    HOF: "head_of_family",
+    ADMIN: "admin",
+};
+
+export const MAX_UPLOAD_SIZE_BYTES = 5 * 1024 * 1024; // 5MB
